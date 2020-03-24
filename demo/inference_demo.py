f14e90eb9691712d7849196739d5961e03353729
Nuri-benbarka <nuribenbarka@gmail.com>
2020-03-24 13:32:45 +0100

    style: made paths relative to repo

--- demo/inference_demo.py
+++ demo/inference_demo.py
@@ -5,15 +5,15 @@ import mmcv
 from os import listdir
 from os.path import isfile, join
 
-config_file = '/home/benbarka/mmdetection/configs/fouriernet/fourier_768_1x_r50_36_60.py'
+config_file = '../configs/fouriernet/fourier_768_1x_r50_36_60.py'
 # download the checkpoint from model zoo and put it in `checkpoints/`
-checkpoint_file = '/home/benbarka/mmdetection/configs/fouriernet/fourier_768_1x_r50_36_60.pth'
+checkpoint_file = '../configs/fouriernet/fourier_768_1x_r50_36_60.pth'
 
 # build the model from a config file and a checkpoint file
 model = init_detector(config_file, checkpoint_file, device='cuda:0')
 
-images_path = '/home/benbarka/PolarMask/samples'
-results_path = '/home/benbarka/PolarMask/results'
+images_path = '../samples/'
+results_path = '../results/'
 images_folder = [f for f in listdir(images_path) if isfile(join(images_path, f))]
 
 for image in images_folder:
